11bc018d112775c160937a486bd791efc17f692f
comradebears <sipnevskiy@gmail.com>
2022-07-25 13:36:55 +0300

    Final layout

--- js/main.js
+++ js/main.js
@@ -601,31 +601,30 @@ $(document).ready(function(){
     });
 
 
-    const slider = document.querySelectorAll('.team-wrap');
-    let mouseDown = false;
-    let startX, scrollLeft;
+    // const slider = document.querySelectorAll('.team-wrap');
+    // let mouseDown = false;
+    // let startX, scrollLeft;
 
-    let startDragging = function (e) {
-      mouseDown = true;
-      startX = e.pageX - slider.offsetLeft;
-      scrollLeft = slider.scrollLeft;
-    };
-    let stopDragging = function (event) {
-      mouseDown = false;
-    };
+    // let startDragging = function (e) {
+    //   mouseDown = true;
+    //   startX = e.pageX - slider.offsetLeft;
+    //   scrollLeft = slider.scrollLeft;
+    // };
+    // let stopDragging = function (event) {
+    //   mouseDown = false;
+    // };
 
-    slider.addEventListener('mousemove', (e) => {
-      e.preventDefault();
-      if(!mouseDown) { return; }
-      const x = e.pageX - slider.offsetLeft;
-      const scroll = x - startX;
-      slider.scrollLeft = scrollLeft - scroll;
-    });
+    // slider.addEventListener('mousemove', (e) => {
+    //   e.preventDefault();
+    //   if(!mouseDown) { return; }
+    //   const x = e.pageX - slider.offsetLeft;
+    //   const scroll = x - startX;
+    //   slider.scrollLeft = scrollLeft - scroll;
+    // });
 
-    // Add the event listeners
-    slider.addEventListener('mousedown', startDragging, false);
-    slider.addEventListener('mouseup', stopDragging, false);
-    slider.addEventListener('mouseleave', stopDragging, false);
+    // slider.addEventListener('mousedown', startDragging, false);
+    // slider.addEventListener('mouseup', stopDragging, false);
+    // slider.addEventListener('mouseleave', stopDragging, false);
 
     
 
